facc20cc354192911d24d973f75f84d7e29ccc32
Alberto Avila <albertoavila@gmail.com>
2013-06-17 16:03:10 -0600

    Avoid losing the original data, click to revert to original value

--- src/supermetrico.js
+++ src/supermetrico.js
@@ -12,8 +12,16 @@
     return value * 0.3048;
   };
 
+  var createSpan = function(text, className) {
+    var span = document.createElement("SPAN");
+    if (className)
+      span.className = className;
+    if (text) 
+      span.appendChild(document.createTextNode(text));
+    return span;
+  };
 
-  var lookup = function(node) {
+  var lookup = function(node, parent) {
     if (node.nodeType == Node.TEXT_NODE) {
       var text = node.textContent;
       // ([\s\S]*?) Matchs any prelude, lazily matchs any amount of character (white and not whitespace)
@@ -27,12 +35,15 @@
       var results;
       var replace = "";
       var lastIndex;
+      var matches = [];
       while (results = regex.exec(text)) {
         match = true;
         var prelude = results[1];
-        var value = parseFloat(results[2].replace(',', ''));
+        var value, originalValue = results[2];
+        value = parseFloat(originalValue.replace(',', ''));
         var whitespace = results[3];
-        var unit = results[4];
+        var unit, originalUnit = results[4];
+        unit = originalUnit;
         var ending = results[5];
         replace += prelude;
         if (unit.indexOf("inch") != -1) {
@@ -46,15 +57,49 @@
           unit = "m";
         }
         replace += value + whitespace + unit + ending;
+        var preSpan = createSpan(prelude);
+        var span;
+        (function(convertedText, originalText) {
+          span = createSpan(convertedText, "super-metrico-converted");
+          span.addEventListener("click", function() {
+            if (this.getAttribute("data-super-metrico-original") == "true") {
+              this.innerText = convertedText;
+              this.setAttribute("data-super-metrico-original", "false");
+            } else {
+              this.innerText = originalText;
+              this.setAttribute("data-super-metrico-original", "true");
+            }
+          });
+        })(value + whitespace + unit, originalValue + whitespace + originalUnit);
+
+        var endSpan;
+        if (ending.length > 0)
+          endSpan = createSpan(ending);
+        matches.push({
+          prelude: preSpan,
+          match: span,
+          end: endSpan
+        });
         lastIndex = regex.lastIndex;
       }
       if (!match)
         return;
-      replace += text.substring(lastIndex);
-      node.textContent = replace;
+
+      var container = createSpan(null, "super-metrico-container");
+      for (var i in matches) {
+        var match = matches[i];
+        container.appendChild(match.prelude);
+        container.appendChild(match.match);
+        if (match.end) container.appendChild(match.end);
+      }
+      var closingText = text.substring(lastIndex);
+      if (closingText)
+        container.appendChild(createSpan(closingText));
+
+      parent.replaceChild(container, node);
     } else {
       for (var i in node.childNodes) {
-        lookup(node.childNodes[i]);
+        lookup(node.childNodes[i], node);
       }
     }
   };
