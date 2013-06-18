def69df0c9c37bdf80a44dcd9e3634a177579a8a
Alberto Avila <albertoavila@gmail.com>
2013-06-18 15:57:53 -0600

    Improved units coverage

--- src/supermetrico.js
+++ src/supermetrico.js
@@ -13,11 +13,43 @@ var superMetrico = (function() {
 
 
   var process = function(text) {
+    var makeUnit = function(names, metric, factor) {
+      return {
+        unit: names,
+        metric: metric,
+        convertion: function(value) { return value * factor }
+      }
+    };
+
+    // Source: http://en.wikipedia.org/wiki/Imperial_units#Units
+    var units = [
+      makeUnit("thou|mil|th", "mm", 0.0254),
+      makeUnit("inch|inches|in", "cm", 2.54),
+      makeUnit("foot|feet|ft", "m", 0.3048),
+      makeUnit("yard|yards|yd", "m", 0.914),
+      makeUnit("mile|miles|mi", "km", 1.609344),
+      makeUnit("acre|acres", "ha", 0.4047),
+      //TODO: Volume units makes no sense, are ounces really used for mass measurement instead of volume?
+      //TODO: The ounces and pints seen on the wild, are they the US versions or british?
+      makeUnit("ounce|ounces|oz", "mm", 28.4130625),
+      makeUnit("pint|pints|pt", "l", 0.473176473),
+      makeUnit("quart|quarts|qt", "l", 0.946352946),
+      makeUnit("gallon|gallons|gal", "l", 3.78541178),
+      makeUnit("pound|pounds|lb", "kg", 0.45359237)
+    ];
+
+    var unitsRegex = '';
+    for (var i in units) {
+      var unit = units[i];
+      unitsRegex += '|' + unit.unit;
+    }
+    unitsRegex = "(" + unitsRegex.substring(1) + ")"; //Remove leading pipe
+
     // (\d[\d\,\.]*) Matchs the numeric value
     // \s* Separator whitespace
     // (inch|pund.. etc) matchs the measurement unit
     // Global case Insensitive Multiline search
-    var regex = /(\d[\d\,\.]*)\s*(inche?s?|pounds?|foot|feet|ft)/gim;
+    var regex = new RegExp("(\\d[\\d\\,\\.]*)\\s*" + unitsRegex, "gim");
     var results;
     var container = document.createElement("UL");
     var matches = false;
@@ -27,15 +59,13 @@ var superMetrico = (function() {
       value = parseFloat(originalValue.replace(',', ''));
       var unit, originalUnit = results[2];
       unit = originalUnit;
-      if (unit.indexOf("inch") != -1) {
-        value = inchConverter(value);
-        unit = "cm";
-      } else if (unit.indexOf("pound") != -1) {
-        value = poundConverter(value);
-        unit = "kgs"
-      } else if (unit == "foot" || unit == "feet" || unit == "ft") {
-        value = feetConverter(value);
-        unit = "m";
+      for (var i in units) {
+        var currentUnit = units[i];
+        if (unit.match(currentUnit.unit)) {
+          value = currentUnit.convertion(value);
+          unit = currentUnit.metric;
+          break;
+        }
       }
       var item = document.createElement("LI");
       item.innerText = originalValue + " " + originalUnit + " -> " + value + " " + unit;
